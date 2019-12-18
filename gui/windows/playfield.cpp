bbd2154075b30e082fe355396c247a51a1cb22ed
Petri Kallio <petri.kallio@hel.fi>
2019-12-18 11:49:20 +0200

    runner

--- gui/windows/playfield.cpp
+++ gui/windows/playfield.cpp
@@ -1,16 +1,16 @@
 #include "playfield.hpp"
 
 void Playfield::init_color_pairs() {
-  init_pair(BLACK,  COLOR_BLACK,  COLOR_BLACK);
-  init_pair(WHITE,  COLOR_WHITE,  COLOR_WHITE);
-  init_pair(YELLOW, COLOR_YELLOW, COLOR_YELLOW);
-  init_pair(ORANGE, COLOR_YELLOW, COLOR_RED);
-  init_pair(RED,    COLOR_RED,    COLOR_RED);
-  init_pair(PURPLE, COLOR_RED,    COLOR_BLUE);
-  init_pair(BLUE,   COLOR_BLUE,   COLOR_BLUE);
-  init_pair(CYAN,   COLOR_CYAN,   COLOR_CYAN);
-  init_pair(GREEN,  COLOR_GREEN,  COLOR_GREEN);
-  init_pair(GREY,   COLOR_BLACK,  COLOR_WHITE);
+  init_pair(BLACK_PIECE,    COLOR_BLACK,  COLOR_BLACK);
+  init_pair(WHITE_PIECE,    COLOR_WHITE,  COLOR_WHITE);
+  init_pair(YELLOW_PIECE,   COLOR_YELLOW, COLOR_YELLOW);
+  init_pair(ORANGE_PIECE,   COLOR_YELLOW, COLOR_RED);
+  init_pair(RED_PIECE,      COLOR_RED,    COLOR_RED);
+  init_pair(PURPLE_PIECE,   COLOR_RED,    COLOR_BLUE);
+  init_pair(BLUE_PIECE,     COLOR_BLUE,   COLOR_BLUE);
+  init_pair(CYAN_PIECE,     COLOR_CYAN,   COLOR_CYAN);
+  init_pair(GREEN_PIECE,    COLOR_GREEN,  COLOR_GREEN);
+  init_pair(WHITE_ON_BLACK, COLOR_WHITE,  COLOR_BLACK);
 }
 
 void Playfield::init_window() {
@@ -18,6 +18,7 @@ void Playfield::init_window() {
   int x = (COLS - Playfield::WIN_WIDTH) / 2;
 
   this->playfield = newwin(Playfield::WIN_HEIGHT, Playfield::WIN_WIDTH, y, x);
+  noecho();
   keypad(this->playfield, true);
   box(this->playfield, 0, 0);
 }
@@ -25,6 +26,9 @@ void Playfield::init_window() {
 Playfield::Playfield() {
   this->init_color_pairs();
   this->init_window();
+
+  this->current_col = START_COL;
+  this->current_row = START_ROW;
 }
 
 Playfield::~Playfield() {
@@ -44,4 +48,27 @@ void Playfield::add_row() {
   this->visualPieces[0].selected = true;
 
   this->current_row += Playfield::ROW_INC;
+}
+
+void Playfield::highlight(int y, int x) {
+  wattron(this->playfield, COLOR_PAIR(WHITE_ON_BLACK));
+  mvwaddch(this->playfield, y, x - 1, '[');
+  mvwaddch(this->playfield, y, x + 1, ']');
+  wattron(this->playfield, COLOR_PAIR(WHITE_ON_BLACK));
+
+  this->refresh();
+}
+
+void Playfield::handle_input(char ch) {
+
+}
+
+void Playfield::run() {
+  while (1) {
+    this->highlight(current_row, current_col);
+
+    char ch = wgetch(this->playfield);
+
+    this->handle_input(ch);
+  }
 }
--- gui/windows/playfield.hpp
+++ gui/windows/playfield.hpp
@@ -5,22 +5,22 @@
 #include "window.hpp"
 #include "../visualpiece.hpp"
 
-#define BLACK   1
-#define WHITE   2
-#define YELLOW  3
-#define ORANGE  4
-#define RED     5
-#define PURPLE  6
-#define BLUE    7
-#define CYAN    8
-#define GREEN   9
-#define GREY    10
+#define BLACK_PIECE     1
+#define WHITE_PIECE     2
+#define YELLOW_PIECE    3
+#define ORANGE_PIECE    4
+#define RED_PIECE       5
+#define PURPLE_PIECE    6
+#define BLUE_PIECE      7
+#define CYAN_PIECE      8
+#define GREEN_PIECE     9
+#define WHITE_ON_BLACK  10
 
 static const int N_PIECES                     = 4;
 static const int N_PIECE_COLORS               = 9;
 static const int PIECE_COLORS[N_PIECE_COLORS] = {
-  BLACK, WHITE, YELLOW, ORANGE,
-  RED, PURPLE, BLUE, CYAN, GREEN
+  BLACK_PIECE, WHITE_PIECE, YELLOW_PIECE, ORANGE_PIECE,
+  RED_PIECE, PURPLE_PIECE, BLUE_PIECE, CYAN_PIECE, GREEN_PIECE
 };
 
 class Playfield: public Window {
@@ -34,15 +34,19 @@ class Playfield: public Window {
   private:
     WINDOW* playfield;
     int current_row;
+    int current_col;
     VisualPiece visualPieces[N_PIECES];
     void init_color_pairs();
     void init_window();
+    void highlight(int y, int x);
+    void handle_input(char ch);
 
   public:
     Playfield();
     ~Playfield();
     void refresh();
     void add_row();
+    void run();
 };
 
 #endif /* PLAYFIELD_HPP */
